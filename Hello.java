d9d5b0ef353c2003a52fda92abecc43136d8402e
Virendra <rajpurohitvirendra11@gmail.com>
2020-11-21 17:59:26 +0530

    commit11

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ public class Hello{
 public static void main(String args[])
 {int x=0;
 x=5/0;
-System.out.println("Hello boss,aa jgduagduiidaa 757 yes");
+System.out.println("Hello boss,aa jgduagduiidaa aa ja hai 757 yes");
 }
 }
